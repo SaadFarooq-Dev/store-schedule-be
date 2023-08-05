4f64182a52728fb635311c8c1dadf0b2386fb6cc
Saad Farooq <saadfarooq.info@gmail.com>
2023-08-05 06:43:55 +0500

    upadte

--- src/index.js
+++ src/index.js
@@ -18,5 +18,8 @@ app.use('/', router)
 
 app.use(errorHandler)
 
+const port = process.env.PORT || 8080
+
+app.listen(port, () => console.log(`server is running on port ${port}`))
 
 export default app
